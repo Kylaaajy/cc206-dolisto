f37aa12d74f7748b9eaf317a15efd65442aa48eb
Hazel_Petate <hazel.petate@wvsu.edu.ph>
2023-11-24 12:53:25 +0800

    fixing the merging error

--- lib/Components/home_drawer.dart
+++ lib/Components/home_drawer.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-//deleted_task_Sermonia_Ordinario
 import '../features/delete_task.dart';
 import '../main.dart';
-=======
 import '../features/add_task.dart';
-
 
 class HomeDrawer extends StatelessWidget {
   const HomeDrawer({Key? key}) : super(key: key);
@@ -13,7 +10,6 @@ class HomeDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: Container(
-// deleted_task_Sermonia_Ordinario
         color: Colors.lightBlue,
         child: ListView(
           children: [
@@ -37,46 +33,29 @@ class HomeDrawer extends StatelessWidget {
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0),
               ),
-              contentPadding: EdgeInsets.all(16.0), 
+              contentPadding: EdgeInsets.all(16.0),
             ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-DeletedTask() {
-}
-=======
-        color: Colors.lightBlue, // Set the background color of the drawer
-        child: ListView(
-          children: [
-            Container(
-              padding: EdgeInsets.all(16.0),
-              child: ListTile(
-                title: Text(
-                  'Add Task',
-                  style: TextStyle(
-                    color:
-                        Colors.white, // Set the text color of the drawer item
-                    fontSize: 18.0,
-                    fontFamily: 'CustomFont', // Apply the custom font
-                  ),
+            ListTile(
+              title: Text(
+                'Add Task',
+                style: TextStyle(
+                  color: Colors.white, // Set the text color of the drawer item
+                  fontSize: 18.0,
+                  fontFamily: 'CustomFont', // Apply the custom font
                 ),
-                onTap: () {
-                  Navigator.pop(context); // Close the drawer
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => AddTask()),
-                  );
-                },
-                tileColor:
-                    Colors.blue, // Set the background color of the ListTile
-                shape: RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.circular(10.0), // Apply border radius
-                ),
+              ),
+              onTap: () {
+                Navigator.pop(context); // Close the drawer
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AddTask()),
+                );
+              },
+              tileColor:
+                  Colors.blue, // Set the background color of the ListTile
+              shape: RoundedRectangleBorder(
+                borderRadius:
+                    BorderRadius.circular(10.0), // Apply border radius
               ),
             ),
           ],
@@ -85,4 +64,3 @@ DeletedTask() {
     );
   }
 }
-
