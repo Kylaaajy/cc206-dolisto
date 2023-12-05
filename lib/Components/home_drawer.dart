ff2f7ffcdefdb4f5ac3077bb8991b30130110b5b
journeymariz.semonia@wvsu.edu.ph <journeymariz.semonia@wvsu.edu.ph>
2023-12-05 18:33:10 +0800

    edited the deleted task page

--- lib/Components/home_drawer.dart
+++ lib/Components/home_drawer.dart
@@ -1,66 +1,93 @@
 import 'package:flutter/material.dart';
-import '../features/delete_task.dart';
-import '../main.dart';
-import '../features/add_task.dart';
+import 'package:cc206_dolisto/Features/task_list_screen.dart';
+import 'package:cc206_dolisto/Features/delete_task.dart';
+import 'package:cc206_dolisto/Features/database_helper.dart';
 
 class HomeDrawer extends StatelessWidget {
-  const HomeDrawer({Key? key}) : super(key: key);
+  final Function(Task) onTaskAdded;
+  final Function(List<Task>) onTasksDeleted;
+  final VoidCallback onAddTaskTap;
+  final VoidCallback onTaskListTap;
+  final Function() onDeletedTaskListTap;
+
+
+  HomeDrawer({
+    required this.onTaskAdded,
+    required this.onTasksDeleted,
+    required this.onAddTaskTap,
+    required this.onTaskListTap,
+    required this.onDeletedTaskListTap,
+
+  });
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Container(
-        color: Colors.lightBlue,
-        child: ListView(
-          children: [
-            ListTile(
-              title: Text(
-                'Deleted Task',
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 18.0,
-                  fontFamily: 'CustomFont',
-                ),
-              ),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => DeletedTask()),
-                );
-              },
-              tileColor: Colors.blue,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              contentPadding: EdgeInsets.all(16.0),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.pinkAccent,
             ),
-            ListTile(
-              title: Text(
-                'Add Task',
-                style: TextStyle(
-                  color: Colors.white, // Set the text color of the drawer item
-                  fontSize: 18.0,
-                  fontFamily: 'CustomFont', // Apply the custom font
-                ),
-              ),
-              onTap: () {
-                Navigator.pop(context); // Close the drawer
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AddTask()),
-                );
-              },
-              tileColor:
-                  Colors.blue, // Set the background color of the ListTile
-              shape: RoundedRectangleBorder(
-                borderRadius:
-                    BorderRadius.circular(10.0), // Apply border radius
+            child: Text(
+              'Menu',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ],
-        ),
+          ),
+          ListTile(
+            title: Text(
+              'Task List',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: onTaskListTap,
+          ),
+          ListTile(
+            title: Text(
+              'Deleted Tasks',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () async {
+              Navigator.pop(context); // Close the drawer
+              final deletedTasks = await _loadDeletedTasks();
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DeletedTaskScreen(
+                    deletedTasks: deletedTasks,
+                    onDeletedTaskListTap: onDeletedTaskListTap,
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
+  }
+}
+
+Future<List<Task>> _loadDeletedTasks() async {
+  try {
+    final DatabaseHelper databaseHelper = DatabaseHelper.instance;
+    final List<Map<String, dynamic>> maps =
+        await (await databaseHelper.database).query('deleted_tasks');
+
+    return List.generate(maps.length, (i) {
+      return Task.fromMap(maps[i]);
+    });
+  } catch (e) {
+    print('Failed to load deleted tasks: $e');
+    return [];
   }
 }
--- lib/features/delete_task.dart
+++ lib/features/delete_task.dart
@@ -1,36 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:cc206_dolisto/Features/database_helper.dart';
+import 'package:cc206_dolisto/Features/task_list_screen.dart';
+import 'package:cc206_dolisto/Components/home_drawer.dart';
 
-class DeleteTask extends StatelessWidget {
-  const DeleteTask({Key? key});
+class DeletedTaskScreen extends StatefulWidget {
+  final List<Task> deletedTasks;
+  final VoidCallback onDeletedTaskListTap;
+
+  DeletedTaskScreen({
+    required this.deletedTasks,
+    required this.onDeletedTaskListTap,
+  });
+
+  @override
+  _DeletedTaskScreenState createState() => _DeletedTaskScreenState();
+}
+
+class _DeletedTaskScreenState extends State<DeletedTaskScreen> {
+  final DatabaseHelper databaseHelper = DatabaseHelper.instance;
+  List<Task> deletedTasks = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadDeletedTasks();
+  }
+
+  Future<void> _loadDeletedTasks() async {
+    final List<Map<String, dynamic>> maps =
+        await databaseHelper.queryTasks('deleted_tasks');
+
+    setState(() {
+      deletedTasks = List.generate(maps.length, (i) {
+        return Task.fromMap(maps[i]);
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-            appBar: AppBar(
-        title: Text("Deleted Task"),
+      appBar: AppBar(
+        title: Text(
+          'Deleted Tasks',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.pinkAccent,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start, // Align children to the start of the column
-        children: [
-          Row(
-            children: [
-              Checkbox(
-                value: false,
-                onChanged: (value) {},
-              ),
-              Text('Clean'),
-            ],
-          ),
-          Row(
-            children: [
-              Text('Yesterday, 8:00 am'),
-              Spacer(), // Add space to push the delete icon to the end
-              Icon(Icons.delete),
-              Text('Delete'),
-            ],
-          ),
-        ],
+      drawer: HomeDrawer(
+        onTaskAdded: (_) {},
+        onTasksDeleted: (_) {},
+        onAddTaskTap: () {
+          Navigator.pushNamed(context, '/add');
+        },
+        onTaskListTap: () {
+          Navigator.pushReplacementNamed(context, '/tasklistscreen');
+        },
+        onDeletedTaskListTap: widget.onDeletedTaskListTap,
+        onCalendarTap: () {
+          Navigator.pushNamed(context, '/calendar');
+        },
+      ),
+      body: ListView.builder(
+        itemCount: deletedTasks.length,
+        itemBuilder: (context, index) {
+          final task = deletedTasks[index];
+          return ListTile(
+            title: Text(task.name),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Details: ${task.details}'),
+                Text('Due Date: ${task.dueDate.toLocal()}'),
+              ],
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.restore),
+              onPressed: () {
+                _restoreTask(task);
+              },
+            ),
+          );
+        },
       ),
     );
+  }
+
+  void _restoreTask(Task task) async {
+    try {
+      // Add the task back to the tasks table in the database
+      await (await databaseHelper.database).insert('tasks', task.toMap());
+
+      // Remove the task from the deletedTasks list
+      setState(() {
+        widget.deletedTasks.remove(task);
+      });
+
+      // Inform the parent screen about the restoration
+      widget.onDeletedTaskListTap();
+    } catch (e) {
+      // Handle any potential exceptions during restoration
+      print('Failed to restore task: $e');
+    }
   }
 }
